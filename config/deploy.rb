93276f8b839712b1d1868b8d7ebcf5258734026d
Tony Hsu <t153426789@gmail.com>
2016-08-19 16:17:17 +0800

    Fix deploy typo

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,7 @@ append :linked_files, 'config/database.yml', 'config/secrets.yml', '.env'
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.3.2'
+set :rbenv_ruby, '2.3.0'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
